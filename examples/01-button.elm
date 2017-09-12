db042f8bf148068a5581f7329598908ca66dec8b
vietkungfu <vietbktn@gmail.com>
2017-09-12 15:22:39 +0900

    Solution 01-Button

--- examples/01-button.elm
+++ examples/01-button.elm
@@ -30,6 +30,7 @@ model =
 type Msg
   = Increment
   | Decrement
+  | Reset
 
 
 update : Msg -> Model -> Model
@@ -41,7 +42,8 @@ update msg model =
     Decrement ->
       model - 1
 
-
+    Reset ->
+      0
 
 -- VIEW
 
@@ -52,4 +54,5 @@ view model =
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , div [] [ button [ onClick Reset ] [ text "Reset"]]
     ]
